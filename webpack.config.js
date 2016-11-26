4db33d77c02eb866c768299f7679770e51b14ff3
hanocha <saiki@feedforce.jp>
2016-11-26 14:10:07 +0900

    fix brackets and indents

--- webpack.config.js
+++ webpack.config.js
@@ -3,15 +3,17 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: './dist'
-  }
+  },
 
   module: {
     loaders: [
-      test: /.jsx?$/,
-      loader: 'babel-loader',
-      exclude: /node-modules/,
-      query: {
-        presets: ['es2015','react']
+      {
+        test: /.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node-modules/,
+        query: {
+          presets: ['es2015','react']
+        }
       }
     ]
   }
